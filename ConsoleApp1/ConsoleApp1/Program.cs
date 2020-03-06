cd637896e0aafd53bdfcf7f21781da60bc594722
tapiwa <tapiwa@email.com>
2020-03-06 10:06:02 +0000

    bubble sort solution

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = { 2, 3, 1 };
+            int[] test2 = { 3, 2, 1 };
+            // removeDuplicates("Hello I I I am am am Chris");
+            //bubbleSort(test);
+            bubble2(test2);
+        }
+
+  
+
+      
+
+        public static void bubble2(int[] nums)
+        {
+            int temp = 0;
+            for(int i = 0; i<nums.Length; i++)
+            {
+                for(int j = i+1; j<nums.Length; j++)
+                {
+                    if(nums[i] > nums[j])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+            foreach(var n in nums)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
