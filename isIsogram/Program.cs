cf7a6f46684fde5b506aef141b3833f8adc1784f
ttiyemba <tapiwatiyemba@icloud.com>
2020-03-25 17:11:20 +0000

    solution added

---
+++ isIsogram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            System.Console.WriteLine(IsIsogram("hello"));
+
+            
+        }
+
+        public static bool IsIsogram(string word){
+        return word.Where(char.IsLetter).GroupBy(char.ToLower).All(t => t.Count() ==1)
+        }
+
+    }
+}
