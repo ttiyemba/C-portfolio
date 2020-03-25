32c210b47dce14bbb76c9eb466246d986affb7e3
ttiyemba <tapiwatiyemba@icloud.com>
2020-03-25 17:05:08 +0000

    smooth solution added

---
+++ Smooth_Setences/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Smooth_Setences
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the sentance you would like to be checked:");
+            String input = Console.ReadLine();
+            String output = "False";
+            string[] arrayInput = input.Split(" ");
+
+            for(int i = 0; i<arrayInput.Length-1;i++){
+                int len = arrayInput[i].Length;
+                Console.WriteLine(arrayInput[i][len-1]);
+                try{
+
+                   if( arrayInput[i][len-1].Equals(arrayInput[i+1][0])){
+                        output = "True";
+
+                    }
+                }catch{
+
+                }
+
+            }
+            Console.WriteLine(output);
+
+        }
+    }
+
+}
