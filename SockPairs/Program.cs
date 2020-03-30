a70f869016fe97933d8d20a2b3ff53578c339344
ttiyemba <tapiwatiyemba@icloud.com>
2020-03-30 19:02:13 +0100

    sockspair solution

---
+++ SockPairs/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace SockPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(Socks("aabccc"));
+
+        }
+
+        public static int Socks(string pairs)
+        {
+
+            int count = 0;
+
+
+            HashSet<char> dist = new HashSet<char>();
+
+            for (int i = 0; i < pairs.Length; i++)
+            {
+
+                if (!dist.Contains(pairs[i]))
+                {
+                    dist.Add(pairs[i]);
+                }
+                else
+                {
+                    count++;
+                    dist.Remove(pairs[i]);
+                }
+
+
+
+
+            }
+            return count;
+        }
+    }
+}
+
+
+
+
+
+
